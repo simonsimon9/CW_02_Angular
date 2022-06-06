484a088737bff8d90428b0d0b84bc5f8f257bc08
simonsimon9 <simonkuang9@outlook.com>
2022-06-05 22:03:50 -0400

    custom pipe fix

--- src/app/app.fahrenheitPipe.ts
+++ src/app/app.fahrenheitPipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 export class FahrenheitPipe implements PipeTransform{
     transform(celsius: number):string {
-        let fah = celsius * 9 / 32 + 32;
+        let fah = (celsius * 9 / 5 )+ 32;
         let output = fah.toString() + " " + `\u2109`; //unicode for fahrenheit
         return output;
         
