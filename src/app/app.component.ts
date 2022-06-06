b6658151c06ff850fd58c300713e109db712bcbe
simonsimon9 <simonkuang9@outlook.com>
2022-06-05 21:41:28 -0400

    Conversion C to F service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
+import { ConversionCtoF} from './ConversionCtoFService';
+
 import { MyDataService } from './myDataService';
+
 @Component({
     selector: 'app-root',
     template: `<h1>Hello world! {{title}}</h1>
-    <ul>{{myname}}</ul>`,
+    <ul>{{f}}</ul>`,
     // 'providers' allows you to create and pass an instance
     // of the class to the constructor header.
-    providers: [MyDataService]
+    providers: [MyDataService , ConversionCtoF ]
 })
 export class AppComponent {
     public title = 'This is Angular!';
-    myname: string; //if this is not assigned at constructor. it will scream to be intialized. 
-    
-    //Create instance of 'MyDataService' right in the constructor
+    f: number;
+    //Create instance of 'ConversionService' right in the constructor
     //header.
-    constructor(myDataService: MyDataService){
-      this.myname = myDataService.getmyNames();
+    constructor(conversionCtoF: ConversionCtoF){
+      this.f = conversionCtoF.getConversion();
     }
    
     
