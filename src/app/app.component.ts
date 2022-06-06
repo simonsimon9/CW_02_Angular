1692a9b3ab95633fa854ce7dc8e847623900dfa7
simonsimon9 <simonkuang9@outlook.com>
2022-06-05 21:14:03 -0400

    service return array of names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { MyDataService } from './myDataService';
 @Component({
-  selector: 'app-root',
-  template: `<h1>Hello world! {{title}}</h1>
-              <second-directive [firstName]="author"></second-directive>
-             
-              `,
-  //'providers' allow you to create and pass an instance 
-  //of the class tot he constructor header
-  providers: [MyDataService]
+    selector: 'app-root',
+    template: `<h1>Hello world! {{title}}</h1>
+    <ul><li *ngFor="let name of names">{{name}}</li></ul>`,
+    // 'providers' allows you to create and pass an instance
+    // of the class to the constructor header.
+    providers: [MyDataService]
 })
 export class AppComponent {
-  public title = 'This is Angular!';
-  public author = "Joyce.";
- 
+    public title = 'This is Angular!';
+    names: Array<any>;
+    // Create instance of 'MyDataService' right in the constructor
+    // header.
+    constructor(myDataService: MyDataService) {
+    // Use service to call getNames() method.
+    this.names = myDataService.getNames();
+    }
 }
